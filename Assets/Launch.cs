f9421a0d458797b153c29ee52c8fa53b1f2440bb
djmp_09 <djmp_09@yahoo.com>
2018-01-16 13:08:12 +0800

    Added Assets Folder

---
+++ Assets/Launch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launch : MonoBehaviour {
+
+	private void OnCollisionEnter (Collision col) {
+		Renderer rend = GetComponent<Renderer>();
+		Color color = Color.green;
+		if(col.gameObject.name == "Rocket Ship")
+        {
+        	rend.material.color = color;
+        }
+        
+    }
+
+
+}
---
+++ Assets/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour {
+
+	Rigidbody _rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		_rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ProcessInput();
+	}
+
+	private void ProcessInput(){
+		if(Input.GetKey(KeyCode.Space)){
+			_rigidBody.AddRelativeForce(Vector3.up *45 * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.A)){
+			transform.Rotate(Vector3.forward*100* Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.Rotate(-Vector3.forward*100 * Time.deltaTime);
+		}
+	}
+
+	private void OnCollisionEnter (Collision col) {
+		if(col.gameObject.name != "Target Pad" && col.gameObject.name != "Launchpad")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
