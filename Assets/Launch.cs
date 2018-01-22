d11084e20ee386005bd0c168cf41667e6f11f1e7
djmp_09 <djmp_09@yahoo.com>
2018-01-23 00:14:07 +0800

    Added 2 more levels

--- Assets/Launch.cs
+++ Assets/Launch.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Launch : MonoBehaviour {
 
-	private void OnCollisionEnter (Collision col) {
-		Renderer rend = GetComponent<Renderer>();
-		Color color = Color.green;
-		if(col.gameObject.name == "Rocket Ship")
-        {
-        	rend.material.color = color;
-        }
-        
+	float dist1;
+	public float launch_dist = 10f;
+	GameObject rocket1;
+	Renderer rend;
+	Color color;
+	Color color1;
+	Color color2;
+	Scene scene;
+	string scene_name;
+
+    void Start()
+    {	
+    	rend = GetComponent<Renderer>();
+		color = Color.green;
+		color1 = Color.blue;
+		color2 = Color.red;
+    	rocket1 = GameObject.Find("Rocket Ship");
+    	dist1 = 0f;
+    	scene = SceneManager.GetActiveScene();
+    	scene_name = scene.name;
     }
 
+    void Update () {
+    	dist1 = Vector3.Distance(rocket1.transform.position, transform.position);
 
+    	if(dist1 < launch_dist){
+    		rend.material.color = color1;
+    	} else {
+    		rend.material.color = color2;
+    	}
+
+    	if(dist1 < 1.7){
+    		if(scene_name == "Level1"){
+    			//SceneManager.UnloadScene("Level1");
+    			SceneManager.LoadScene("Level2");		
+    		} else if(scene_name == "Level2"){
+    			//SceneManager.UnloadScene("Level2");
+    			SceneManager.LoadScene("Level3");		
+    		} else {
+    			rend.material.color = color;		
+    		}
+    		
+    	}
+
+    }
 }
--- Assets/Rocket2.cs
+++ Assets/Rocket2.cs
@@ -3,20 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Rocket : MonoBehaviour {
-	public GameObject rocket1;
+public class Rocket2 : MonoBehaviour {
+	public GameObject rocket3;
 	Rigidbody _rigidBody;
 	AudioSource _audioSource;
 	bool _isSoundPlaying = false;
 	//float speed = 5f;
 	public float mainThrust = 45f;
 	public float rcsThrust = 100f;
-	bool isCollision = true;
 	// Use this for initialization
 	void Start () {
 		_rigidBody = GetComponent<Rigidbody>();
 		_audioSource = GetComponent<AudioSource>();
-		rocket1 = GameObject.Find("Rocket Ship");
+		rocket3 = GameObject.Find("Rocket Ship 2");
 	}
 	
 	// Update is called once per frame
@@ -25,7 +24,7 @@ public class Rocket : MonoBehaviour {
 	}
 
 	private void ProcessInput(){
-		if(Input.GetKey(KeyCode.Space)){
+		if(Input.GetKey(KeyCode.UpArrow)){
 			_rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
 			if(!_isSoundPlaying){
 				_audioSource.Play();
@@ -35,27 +34,18 @@ public class Rocket : MonoBehaviour {
 				_audioSource.Stop();
 				_isSoundPlaying = false;
 		}
-		if(Input.GetKey(KeyCode.A)){
+		if(Input.GetKey(KeyCode.LeftArrow)){
 			transform.Rotate(Vector3.forward * rcsThrust * Time.deltaTime);
 		}
-		if(Input.GetKey(KeyCode.D)){
+		if(Input.GetKey(KeyCode.RightArrow)){
 			transform.Rotate(-Vector3.forward * rcsThrust * Time.deltaTime);
-		}
-		if(Input.GetKeyDown(KeyCode.O)){
-			if(isCollision){
-				isCollision = false;
-			} else {
-				isCollision = true;
-			}
 		}
 	}
 
 	private void OnCollisionEnter (Collision col) {
-		if(isCollision){
-			if(col.gameObject.name != "Target Pad" && col.gameObject.name != "Launchpad")
-	        {
-	            SceneManager.LoadScene("Level1");
-	        }
-    	}
+		if(col.gameObject.name != "Target Pad" && col.gameObject.name != "Launchpad 2")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
